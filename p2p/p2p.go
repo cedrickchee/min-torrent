60f586b57dfd86b41d99dfb02b1c06362cba7d00
Cedric Chee <cedric@invictusbyte.com>
2020-01-14 15:54:52 +0800

    Clean up

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -40,14 +40,6 @@ type pieceResult struct {
 	buf   []byte
 }
 
-/*type swarm struct {
-	clients    []*client
-	queue      chan *pieceWork
-	buf        []byte
-	piecesDone int
-	mux        sync.Mutex
-}*/
-
 // Download downloads a torrent
 func (t *Torrent) Download() ([]byte, error) {
 	numPieces := len(t.PieceHashes)
@@ -80,18 +72,6 @@ func (t *Torrent) Download() ([]byte, error) {
 	close(workQueue)
 
 	return buf, nil
-
-	// clients := d.initClients()
-	// if len(clients) == 0 {
-	// 	return nil, fmt.Errorf("Could not connect to any of %d clients", len(d.Peers))
-	// }
-	// log.Printf("Connected to %d clients\n", len(clients))
-
-	// queue := make(chan *pieceWork, len(t.PieceHashes))
-	// for index, hash := range t.PieceHashes {
-	// 	queue <- &pieceWork{index, hash}
-	// }
-	// return t.processQueue(clients, queue), nil
 }
 
 func (t *Torrent) downloadWorker(peer Peer, workQueue chan *pieceWork, results chan *pieceResult) {
@@ -133,7 +113,6 @@ func (t *Torrent) downloadWorker(peer Peer, workQueue chan *pieceWork, results c
 }
 
 func attemptDownloadPiece(c *client, pw *pieceWork) ([]byte, error) {
-	// Previously known as downloadPiece
 	pieceLength := pw.length
 	buf := make([]byte, pieceLength)
 	downloaded := 0
@@ -192,8 +171,6 @@ func attemptDownloadPiece(c *client, pw *pieceWork) ([]byte, error) {
 		}
 		if msg.ID != message.MsgPiece {
 			log.Println(msg)
-		} else {
-			// log.Println("Received", len(msg.Payload), "bytes")
 		}
 
 		switch msg.ID {
@@ -217,110 +194,6 @@ func attemptDownloadPiece(c *client, pw *pieceWork) ([]byte, error) {
 	}
 	return buf, nil
 }
-
-/*func (t *Torrent) initClients() []*client {
-	// Create clients in parallel
-	c := make(chan *client)
-	for _, p := range t.Peers {
-		go func(p Peer) {
-			client, err := newClient(p, t.PeerID, t.InfoHash)
-			if err != nil {
-				c <- nil
-			} else {
-				c <- client
-			}
-		}(p)
-	}
-
-	// Gather clients into a slice
-	clients := make([]*client, 0)
-	for range t.Peers {
-		client := <-c
-		if client != nil {
-			clients = append(clients, client)
-		}
-	}
-
-	return clients
-}
-
-func (t *Torrent) processQueue(clients []*client, queue chan *pieceWork) []byte {
-	s := swarm{
-		clients: clients,
-		queue:   queue,
-		buf:     make([]byte, t.Length),
-		mux:     sync.Mutex{},
-	}
-
-	numWorkers := (len(s.clients) + 1) / 2
-	log.Printf("Spawning %d workers\n", numWorkers)
-	wg := sync.WaitGroup{}
-	wg.Add(numWorkers)
-
-	for i := 0; i < numWorkers; i++ {
-		go s.worker(t, &wg)
-	}
-
-	wg.Wait()
-	return s.buf
-}
-
-func (s *swarm) selectClient(index int) (*client, error) {
-	for _, c := range s.clients {
-		if !c.engaged && !c.choked && c.hasPiece(index) {
-			return c, nil
-		}
-	}
-	for _, c := range s.clients {
-		if !c.engaged && c.hasPiece(index) {
-			return c, nil
-		}
-	}
-	return nil, fmt.Errorf("Could not find client for piece %d", index)
-}
-
-func (s *swarm) worker(t *Torrent, wg *sync.WaitGroup) {
-	for pw := range s.queue {
-		s.mux.Lock()
-		c, err := s.selectClient(pw.index)
-		if err != nil {
-			// fmt.Println("Error:", err)
-			// Re-enqueue the piece to try again
-			s.queue <- pw
-			s.mux.Unlock()
-			continue
-		}
-		c.engaged = true
-		s.mux.Unlock()
-
-		begin, end := calculateBoundsForPiece(pw.index, len(t.PieceHashes), t.Length)
-		pieceLength := end - begin
-		pieceBuf, err := downloadPiece(c, pw, pieceLength)
-		if err != nil {
-			// Re-enqueue the piece to try again
-			log.Println(err)
-			s.removeClient(c)
-			s.queue <- pw
-		} else {
-			// Copy into buffer should not overlap with other workers
-			copy(s.buf[begin:end], pieceBuf)
-
-			s.mux.Lock()
-			s.piecesDone++
-			log.Printf("Downloaded piece %d (%d/%d) %0.2f%%\n", pw.index, s.piecesDone, len(t.PieceHashes), float64(s.piecesDone)/float64(len(t.PieceHashes))*100)
-			s.mux.Unlock()
-
-			if s.piecesDone == len(t.PieceHashes) {
-				close(s.queue)
-			}
-		}
-
-		s.mux.Lock()
-		c.engaged = false
-		s.mux.Unlock()
-	}
-	wg.Done()
-}*/
 
 func calculateBoundsForPiece(index, numPieces, length int) (begin int, end int) {
 	pieceLength := length / numPieces
@@ -328,101 +201,6 @@ func calculateBoundsForPiece(index, numPieces, length int) (begin int, end int) 
 	end = begin + pieceLength
 	return begin, end
 }
-
-/*func downloadPiece(c *client, pw *pieceWork, pieceLength int) ([]byte, error) {
-	buf := make([]byte, pieceLength)
-	c.unchoke()
-	c.interested()
-	downloaded := 0
-	requested := 0
-	for downloaded < pieceLength {
-		for c.hasNext() {
-			msg, err := c.read() // this call blocks
-			if err != nil {
-				return nil, err
-			}
-			if msg == nil { // keep-alive
-				continue
-			}
-			if msg.ID != message.MsgPiece {
-				fmt.Println(msg)
-			}
-			switch msg.ID {
-			case message.MsgUnchoke:
-				c.choked = false
-			case message.MsgChoke:
-				c.choked = true
-			case message.MsgHave:
-				index, err := message.ParseHave(msg)
-				if err != nil {
-					return nil, err
-				}
-				c.bitfield.SetPiece(index)
-			case message.MsgPiece:
-				n, err := message.ParsePiece(pw.index, buf, msg)
-				if err != nil {
-					return nil, err
-				}
-				downloaded += n
-			}
-		}
-
-		if !c.choked && requested < pieceLength && requested-downloaded <= queueSize+1 {
-			for i := 0; i < queueSize; i++ {
-				blockSize := maxBlockSize
-				if pieceLength-requested < blockSize {
-					// Last block might be shorter than the typical block
-					blockSize = pieceLength - requested
-				}
-				// fmt.Println("Request")
-				c.request(pw.index, requested, blockSize)
-				requested += blockSize
-			}
-		}
-
-		msg, err := c.read() // this call blocks
-		if err != nil {
-			return nil, err
-		}
-		if msg == nil { // keep-alive
-			continue
-		}
-		if msg.ID != message.MsgPiece {
-			fmt.Println(msg)
-		} else {
-			fmt.Println("Received", len(msg.Payload), "bytes")
-		}
-
-		switch msg.ID {
-		case message.MsgChoke:
-			c.choked = true
-		case message.MsgUnchoke:
-			c.choked = false
-		case message.MsgHave:
-			index, err := message.ParseHave(msg)
-			if err != nil {
-				return nil, err
-			}
-			c.bitfield.SetPiece(index)
-		case message.MsgPiece:
-			n, err := message.ParsePiece(pw.index, buf, msg)
-			if err != nil {
-				return nil, err
-			}
-			downloaded += n
-		}
-	}
-
-	c.have(pw.index)
-	c.notInterested()
-
-	err := checkIntegrity(pw, buf)
-	if err != nil {
-		return nil, err
-	}
-
-	return buf, nil
-}*/
 
 func checkIntegrity(pw *pieceWork, buf []byte) error {
 	hash := sha1.Sum(buf)
@@ -432,20 +210,3 @@ func checkIntegrity(pw *pieceWork, buf []byte) error {
 	}
 	return nil
 }
-
-/*func (s *swarm) removeClient(c *client) {
-	if len(s.clients) == 1 {
-		panic("Removed last client")
-	}
-	log.Printf("Removing client. %d clients remaining\n", len(s.clients))
-	s.mux.Lock()
-	c.conn.Close()
-	var i int
-	for i = 0; i < len(s.clients); i++ {
-		if s.clients[i] == c {
-			break
-		}
-	}
-	s.clients = append(s.clients[:i], s.clients[i+1:]...)
-	s.mux.Unlock()
-}*/
